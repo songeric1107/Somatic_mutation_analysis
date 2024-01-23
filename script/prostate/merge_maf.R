0c9c79032aa52d4b67e66547a8b92a8393565c5c
songeric1107 <songeric1107@gmail.com>
2024-01-22 23:26:18 -0500

    Update merge_maf.R

--- script/prostate/merge_maf.R
+++ script/prostate/merge_maf.R
@@ -2,17 +2,9 @@ library(maftools)
 #setwd("/Users/ysong/Desktop/P_Tran_ref/maf/soma/")
 path=getwd()
 clinical_tmb=read.table("/Users/ysong/Desktop/P_Tran_ref/onco_508/soma/meta_no97.txt",sep="\t",header=T,check.names = F)
-file.names <- dir(path, pattern =".maf.mod.txt")
+#file.names <- dir(path, pattern =".maf.mod.txt")
 
-
-maf_3=maftools:::merge_mafs(file.names,clinicalData=clinical_tmb,
-                            vc_nonSyn=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site","Nonsense_Mutation", "Nonstop_Mutation", "In_Frame_Del","In_Frame_Ins", "Missense_Mutation","3'Flank",
-"3'UTR", "5'Flank","5'UTR","Intron","RNA","Silent","IGR","Splice_Region"))
-saveRDS(maf_3,"~/Desktop/maf_all_.var.515.rds")
-
-
-pdf("soma3.pdf")
-setwd("projects-t3/PTRAN/Projects_starting_Jan2022/dnaseq/analysis/DNA/gatk/ysong_test/analysis_foundation_tempus/tempus_IGS_MAF/")
+setwd("/Volumes/projects-t3/PTRAN/Projects_starting_Jan2022/dnaseq/analysis/DNA/gatk/ysong_test/analysis_foundation_tempus/tempus_IGS_MAF/")
 path=getwd()
 file.names <- dir(path, pattern =".maf")
 
@@ -39,6 +31,21 @@ clin=unique(merged_mafs_TN_1000G@data$ClinVar_VCF_CLNSIG)
 sub=subsetMaf(merged_mafs_TN_1000G, query=  "ClinVar_VCF_CLNSIG %in% clin[c(2,4,5,8,11,13,16)]",dropLevels =F)
 
 saveRDS(sub,"non_synonous_tempus.patho.like-patho.patho-conflict.gatk.rds")
+
+merged_mafs_TN_1000G_df <- merged_mafs_TN_1000G@data
+merged_mafs_TN_1000G_sub<-merged_mafs_TN_1000G_df[,c("Hugo_Symbol","Entrez_Gene_Id", "Chromosome", "Start_Position", "End_Position", "Tumor_Sample_Barcode", "Variant_Classification", "Variant_Type","Reference_Allele","Tumor_Seq_Allele1","Tumor_Seq_Allele2", "tumor_f","t_alt_count","t_ref_count","n_alt_count","n_ref_count","dbSNP_RS","cDNA_Change","Protein_Change","ClinVar_VCF_CLNSIG","ClinVar_VCF_ID","COSMIC_overlapping_mutations")]
+
+
+
+merged_mafs_TN_1000G_sub$tot_depth_tumor <- merged_mafs_TN_1000G_sub$t_alt_count + merged_mafs_TN_1000G_sub$t_ref_count
+merged_mafs_TN_1000G_sub$tot_depth_normal <- merged_mafs_TN_1000G_sub$n_alt_count + merged_mafs_TN_1000G_sub$n_ref_count
+merged_mafs_TN_1000G_sub$DNA.Mutations <- paste(merged_mafs_TN_1000G_sub$Hugo_Symbol,merged_mafs_TN_1000G_sub$Protein_Change)
+
+
+merged_mafs_TN_1000G_sub <- merged_mafs_TN_1000G_sub %>% filter(tumor_f < 0.40 & tot_depth_tumor>=10)
+
+sub1=read.maf(merged_mafs_TN_1000G_sub)
+sub1=subsetMaf(sub1, query=  "ClinVar_VCF_CLNSIG %in% clin[c(2,4,5,8,11,13,16)]",dropLevels =F)
 #d <- merge_mafs(lapply(Sys.glob("mafs/Patient*.maf"), read.maf))
 
 # Load sample information
@@ -58,25 +65,26 @@ merged_mafs_TN_1000G_sub$DNA.Mutations <- paste(merged_mafs_TN_1000G_sub$Hugo_Sy
 
 
 merged_mafs_TN_1000G_sub <- merged_mafs_TN_1000G_sub %>% filter(tumor_f < 0.40 & tot_depth_tumor>=10)
+saveRDS(merged_maf_TN_1000G_sub,"/local/projects-t3/PTRAN/Projects_starting_Jan2022/dnaseq/analysis/DNA/gatk/ysong_test/analysis_foundation_tempus/tempus_IGS_MAF.rds")
 
 
-tempus=readRDS("/Users/ysong/Desktop/combine_tempus_foundation/tempus.filter.209.rds")
+#tempus=readRDS("/Users/ysong/Desktop/combine_tempus_foundation/tempus.filter.209.rds")
 
 
 
 ### foundation
 
 
-path=getwd()
+path="/local/projects-t3/PTRAN/Projects_starting_Jan2022/dnaseq/analysis/DNA/gatk/ysong_test/analysis_foundation_tempus/maf/"
 
 list.mafs= list.files(path, pattern=".maf", full.names=T)
 list.all.maf.files <- lapply(list.mafs, function(i){
   read.delim(i, sep = "\t", header = TRUE, fill = TRUE, comment.char = "#")
 })
 
-var.classes=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site","Nonsense_Mutation", "Nonstop_Mutation", "In_Frame_Del","In_Frame_Ins", "Missense_Mutation","3'Flank",
-            "3'UTR", "5'Flank","5'UTR","Intron","RNA","Silent","IGR","Splice_Region","DE_NOVO_START_IN_FRAME", "DE_NOVO_START_OUT_FRAME",
-            "START_CODON_SNP")
+#var.classes=c("Frame_Shift_Del", "Frame_Shift_Ins", "Splice_Site", "Translation_Start_Site","Nonsense_Mutation", "Nonstop_Mutation", "In_Frame_Del","In_Frame_Ins", "Missense_Mutation","3'Flank",
+ #           "3'UTR", "5'Flank","5'UTR","Intron","RNA","Silent","IGR","Splice_Region","DE_NOVO_START_IN_FRAME", "DE_NOVO_START_OUT_FRAME",
+  #          "START_CODON_SNP")
 
 merged_mafs_TN_1000G <- maftools::merge_mafs(list.all.maf.files , vc_nonSyn=var.classes)
 
@@ -94,10 +102,23 @@ merged_mafs_TN_1000G_sub<-merged_mafs_TN_1000G_df[,c("Hugo_Symbol","Entrez_Gene_
 
 #Added columns
 merged_mafs_TN_1000G_sub$tot_depth_tumor <- merged_mafs_TN_1000G_sub$t_alt_count + merged_mafs_TN_1000G_sub$t_ref_count
-#merged_mafs_TN_1000G_sub$tot_depth_normal <- merged_mafs_TN_1000G_sub$n_alt_count + merged_mafs_TN_1000G_sub$n_ref_count
+merged_mafs_TN_1000G_sub$tot_depth_normal <- merged_mafs_TN_1000G_sub$n_alt_count + merged_mafs_TN_1000G_sub$n_ref_count
 merged_mafs_TN_1000G_sub$DNA.Mutations <- paste(merged_mafs_TN_1000G_sub$Hugo_Symbol,merged_mafs_TN_1000G_sub$Protein_Change)
 
-merged_mafs_TN_1000G_sub$Chrom_Pos <- paste(merged_mafs_TN_1000G_sub$Chromosome,merged_mafs_TN_1000G_sub$Start_Position,merged_mafs_TN_1000G_sub$End_Position,merged_mafs_TN_1000G_sub$Reference_Allele,merged_mafs_TN_1000G_sub$Tumor_Seq_Allele2,sep=":")
+
+merged_mafs_TN_1000G_sub <- merged_mafs_TN_1000G_sub %>% filter(tumor_f < 0.40 & tot_depth_tumor>=10)
+saveRDS(merged_maf_TN_1000G_sub,"/local/projects-t3/PTRAN/Projects_starting_Jan2022/dnaseq/analysis/DNA/gatk/ysong_test/analysis_foundation_tempus/foundation_IGS_MAF.rds")
+
+
+
+
+
+##########not use#####
+
+
+
+
+
 library(dplyr)
 found=read.table("foundation_gene.tab",header=F,sep="\t")
 
@@ -110,7 +131,7 @@ merged_mafs_TN_1000G_sub2 <- merged_mafs_TN_1000G_sub %>%
 sub.maf <- merged_mafs_TN_1000G_sub2 %>% filter(type %in% "Foundation" )
 
 sub_filtered <- sub.maf %>% filter(tumor_f < 0.40 & tot_depth_tumor>=10)
-
+saveRDS(sub
 
 tempus=readRDS("/Users/ysong/Desktop/combine_tempus_foundation/tempus.filter.209.rds")
 tempus_gene=read.table("/Users/ysong/Desktop/P_Tran_ref/analysis_result/freebay_plot/820_correct/tab_file/tempus_marker_clean.txt",sep="\t",header=T)
