bf2fd9fb15dee290a0a4e8ebfde856e9edac36bc
songeric1107 <songeric1107@gmail.com>
2025-09-09 18:45:11 -0400

    Update and rename cnv to cnv.sh

---
+++ script/prostate/cnv.sh
@@ -0,0 +1,4 @@
+
+step1_mod.r
+ for i in *.txt.mod.txt;do echo tail -n +2 $i \|cut -f2- \> $i.bed;done>step2.pbs
+for i in *.txt.bed;do echo intersectBed -a $i -b hg19.ref.bed -wa -wb -F 0.5 \>$i.ann.bed;done >step3.ann.50.pbs
