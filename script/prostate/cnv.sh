f10b5c36ed9374302bcbd59691515fb3619bfc8a
songeric1107 <songeric1107@gmail.com>
2025-09-09 19:28:22 -0400

    Update cnv.sh

--- script/prostate/cnv.sh
+++ script/prostate/cnv.sh
@@ -6,4 +6,4 @@ for i in *.noN;do echo tail -n +2 $i \|cut -f2- \> $i.bed;done>step2.pbs
 for i in *.txt.bed;do echo intersectBed -a $i -b hg19.ref.bed -wa -wb -F 0.5 \>$i.ann.bed;done >step3.ann.50.pbs
 for i in *.txt.bed;do echo intersectBed -a $i -b hg19.ref.bed -wa -wb -F 1 \>$i.ann.bed;done >step3.ann.100.pbs
 
- for i in *.noN.bed;do echo intersectBed -a $i -b hg19.ref.bed -wa -wb -F 1\>$i.ann.bed;done >step3.ann.100.pbs
+ for i in *.csv.noN;do echo intersectBed -a $i -b hg19.ref.bed -wa -wb -F 1\>$i.ann.bed;done >step3.ann.100.pbs
